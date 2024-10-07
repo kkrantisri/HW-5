4d96b2be22b982305dc2235e4fcdb3a6fec89a6c
Kranti Sri Kalepalli <157083369+kkrantisri@users.noreply.github.com>
2024-10-07 19:10:23 -0400

    Initial commit

--- answer.py
+++ answer.py
@@ -1,38 +1,96 @@
+"""
+QUESTION 1:
+========================================================================================================
+Given a list of numbers, write a function to find the maximum number in the list.
+Do Not Use the built-in Python function max.
+Note: For the purpose of this problem, we define that an empty list will return None.
+
+NOTE: DO NOT USE THE PYTHON FUNCTION max. WRITE your program using a loop. 
+
+Example 1:
+========================================
+Input: [10, 5, 20, 15, 25]
+Output: 25
+
+Example 2:
+========================================
+Input:[10,10,10]
+Output: 10
+
+Example 3:
+========================================
+Input: []
+Output: None
+"""
+
+def find_maximum(numbers):
 
 
-def tuple():
-    """
-    This is to review tuples and basic operations.
-    """
-    # Assign the first element of the tuple "t1" to the variable x.
-    t1 = (13, 100, 99, 100, 999)
+
+"""
+QUESTION 2: 
+========================================================================================================
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Write a function named generateParenthesis that takes an integer as an input and returns a list of strings 
+as an output. Note that you can define a function inside a function if necessary.
+
+Example 1:
+========================================
+Input: 0
+Output: ['']
+
+Example 2:
+========================================
+Input: 1
+Output: ['()']
+
+Example 3:
+========================================
+Input: 2
+Output: ['(())', '()()']
+
+Example 4:
+========================================
+Input: 3
+Output: ['((()))', '(()())', '(())()', '()(())', '()()()'])
+"""
+
+def generateParenthesis(n):
 
 
-    # Assign the next to the last element of the tuple "t1" to the
-    # variable y. For the given tuple, the next to the last element
-    # is 100.
+"""
+QUESTION 3: 
+========================================================================================================
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Note: For the purpose of this problem, we define empty string as valid palindrome. Write a function
+named isPalindrome that takes a string as an input and returns a bool as an output.
+
+Hint: refer to the following example on how to reverse a string.
+
+>>> S = "abc"
+>>> S[::-1]
+'cba'
 
 
-    # Determine the index of 2 in the tuple "t2" by using the index
-    # method for tuples. Assign the result to the variable z.
+Example 1:
+========================================
+Input: "A man, a plan, a canal: Panama"
+Output: true
+
+Explanation:
+After removing non-alphanumeric charactors and ignoring cases, the input is:  amanaplanacanalPanama
+which reads the same as backward and forward, so it is true.
+
+Example 2:
+=========================================
+Input: "race a car"
+Output: false
+
+Explanation:
+After removing non-alphanumeric charactors and ignoring cases, the input is:  raceacar
+which does not read the same as backward and forward, so it is false.
+"""
+
+def isPalindrome(x):
+
     
-    t2 = (55, 777, 54, 6, 76, 101, 1, 2, 8679, 98, 123, 98, 98)
-
-    # Determine the number of times 98 occurs in the tuple "t2" by
-    # using the count method for tuples. Assign the result to
-    # the variable f.
-    
-
-    # Determine the sum of all the numbers in the tuple "t2" and
-    # assign the result to the variable m.
-    
-
-    # Determine the maximum number of all the numbers
-    # in the tuple "t2" and assign the result to the variable n.
-
-
-    return x, y, z, f, m, n
-
-
-
-tuple()
